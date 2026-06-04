e8e088c850de8da9a244991b89963da1525934bc
hannes557766-ui <226123776+hannes557766-ui@users.noreply.github.com>
2026-06-04 15:12:06 +0200

    Update service worker cache name and URLs

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-const CACHE_NAME = 'untis-clone-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  'https://cdn.tailwindcss.com'
-];
+const CACHE_NAME = 'webuntis-clone-v1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html']))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
